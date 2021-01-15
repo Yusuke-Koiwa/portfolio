673f7d6376d69c9776459a47f2d63252b43ae6d9
Yusuke-Koiwa <smallrock0109@gmail.com>
2021-01-15 20:59:18 +0900

    マークアップ：Top

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <header class="fixed z-50 w-full h-20 px-6 bg-gradient-to-b from-black">
+    <nav class="w-full h-full flex justify-between items-center text-white">
+      <h1 class="font-heading text-4xl">Koiwa's Portfolio</h1>
+      <ul class="flex font-heading text-xl">
+        <li v-for="link in links" :key="link.name" class="px-4">
+          <a :href="link.url">{{ link.name }}</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      links: [
+        {
+          name: 'ABOUT',
+          url: '#about',
+        },
+        {
+          name: 'SKILLS',
+          url: '#skills',
+        },
+        {
+          name: 'SERVICE',
+          url: '#service',
+        },
+        {
+          name: 'WORKS',
+          url: '#works',
+        },
+        {
+          name: 'CONTACT',
+          url: '#contact',
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/Hero/Hero.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="hero relative w-full h-screen px-6 bg-cover bg-no-repeat bg-center">
+    <img
+      src="/static/img/hero/hero-wave-white.png"
+      alt=""
+      class="absolute bottom-0 -right-0.5 w-full h-full"
+    />
+    <p
+      class="absolute catch-copy top-1/2 right-1/2 transform translate-x-3/4 -translate-y-1/2 font-heading text-7xl text-center z-10 whitespace-nowrap"
+    >
+      Expand the Possibilities
+    </p>
+    <img
+      src="/static/img/hero/rocket@2x.png"
+      alt=""
+      class="rocket absolute bottom-0 left-0 h-auto"
+    />
+    <scroll-mark />
+  </section>
+</template>
+
+<script>
+import ScrollMark from './ScrollMark.vue';
+export default {
+  name: 'Hero',
+  components: { ScrollMark },
+};
+</script>
+
+<style scoped>
+.hero {
+  background-image: url('/static/img/hero/hero-bg@2x.jpg');
+}
+
+.catch-copy {
+  text-shadow: 1px 0 10px #fff;
+}
+
+.rocket {
+  width: 600px;
+  /* animation: hovering 2s ease-in-out infinite alternate; */
+}
+@keyframes hovering {
+  0% {
+    transform: translateY(0);
+  }
+
+  100% {
+    transform: translateY(20px);
+  }
+}
+</style>

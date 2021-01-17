666a3834dff8d739a8aa51c0cef22125effac55c
Yusuke-Koiwa <smallrock0109@gmail.com>
2021-01-17 16:06:07 +0900

    Sliderエリア作成

---
+++ src/components/Works/Slider.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="slider-wrapper">
+    <div class="bg-c-gray">aaa</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Slider',
+};
+</script>
+
+<style scoped>
+.slider-wrapper {
+  padding-left: 11%;
+}
+</style>
--- src/components/Works/Works.vue
+++ src/components/Works/Works.vue
@@ -3,14 +3,16 @@
     <Container>
       <Heading title="WORKS" subtitle="作品" class="mb-16" />
     </Container>
+    <Slider />
   </section>
 </template>
 <script>
 import Container from '../Container.vue';
 import Heading from '../Heading.vue';
+import Slider from './Slider.vue';
 export default {
   name: 'Works',
-  components: { Container, Heading },
+  components: { Container, Heading, Slider },
 };
 </script>
 <style></style>

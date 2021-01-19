30db934a3332f933c69625ee2c5461a58e7b0504
Yusuke-Koiwa <smallrock0109@gmail.com>
2021-01-19 18:53:10 +0900

    スライダー修正

--- src/components/Works/LinkButton.vue
+++ src/components/Works/LinkButton.vue
@@ -1,7 +1,10 @@
 <template>
-  <a :href="url" class="button mx-auto flex justify-center items-center shadow-md bg-white dark:bg-black dark-text-white">
-    <div class="text-xl font-semibold">MORE</div>
-    <div></div>
+  <a :href="url" class="button block mb-1 mx-auto flex justify-center items-center shadow-md bg-white dark:bg-black">
+    <div class="mr-2 text-xl font-semibold dark:text-white">MORE</div>
+    <div class="arrow relative pt-1">
+      <div class="line bg-black dark:bg-white"></div>
+      <div class="tip absolute right-0 top-0 bg-black dark:bg-white"></div>
+    </div>
   </a>
 </template>
 
@@ -18,5 +21,14 @@ export default {
 .button {
   width: 160px;
   height: 60px;
+}
+.line {
+  width: 38px;
+  height: 3px;
+}
+.tip {
+  width: 3px;
+  height: 15px;
+  transform: translate(-5px, -7.5px) rotate(-45deg);
 }
 </style>
--- src/components/Works/Slider.vue
+++ src/components/Works/Slider.vue
@@ -6,39 +6,39 @@
         <swiper :slides-per-view="2" :space-between="50" :loop="true" navigation @swiper="onSwiper" @slideChange="onSlideChange">
           <swiper-slide class="relative">
             <img src="/static/img/works/works01@2x.png" alt="" class="mb-9" />
-            <div class="swiper-slide-text">
-              <h3 class="mb-9 text-3xl text-center font-semibold">Anytime Nature</h3>
-              <p class="mb-9 text-lg">
+            <h3 class="slide-text mb-9 text-3xl text-center font-semibold">Anytime Nature</h3>
+            <div class="slide-text mb-9 text-lg">
+              <p>
                 写真を投稿できるWebアプリケーションです。 バックエンドのサーバー処理から、フロント
                 エンドの実装まで全て個人で開発しました。バックエンドはRuby on Rails、画像の保存は AWS S3を使用しています。
               </p>
-              <Link-button :url="'#'" />
             </div>
+            <Link-button :url="'#'" />
           </swiper-slide>
 
           <swiper-slide class="relative">
             <img src="/static/img/works/works02@2x.png" alt="" class="mb-9" />
-            <div class="swiper-slide-text">
-              <h3 class="mb-9 text-3xl text-center font-semibold">完全版マハーバーラタ（模写×WordPress）</h3>
+            <h3 class="mb-9 text-3xl text-center font-semibold">完全版マハーバーラタ（模写×WordPress）</h3>
+            <div class="slide-text">
               <p class="mb-3 text-lg">
                 実際の案件で使用されたXDデザインを元に模写したサイトです。WordPressサイト化し、ニュース、販売スケジュール、キャストなどをクライアント様側
                 で簡単に更新できるようにしました。サイトの確認には認証が必要です。
               </p>
-              <p class="mb-9">ユーザー名：iwa<br />パスワード：iwa</p>
-              <Link-button :url="'#'" />
+              <p class="mb-3">ユーザー名：iwa<br />パスワード：iwa</p>
             </div>
+            <Link-button :url="'#'" />
           </swiper-slide>
 
           <swiper-slide class="relative">
             <img src="/static/img/works/works03@2x.png" alt="" class="mb-9" />
-            <div class="swiper-slide-text">
-              <h3 class="mb-9 text-3xl text-center font-semibold">Engress（WordPress架空企業サイト）</h3>
+            <h3 class="mb-9 text-3xl text-center font-semibold">Engress（WordPress架空企業サイト）</h3>
+            <div class="slide-text">
               <p class="mb-3 text-lg">
                 WordPressを用いて構築した架空の企業サイトです。XDデザインを元にコーディングを行なっております。ブログ、お知らせ、料金プランなどをクライアント様側で簡単に更新できるようにしました。サイトの確認には認証が必要です。
               </p>
-              <p class="mb-9">ユーザー名：iwa<br />パスワード：iwa</p>
-              <Link-button :url="'#'" />
+              <p class="mb-3">ユーザー名：iwa<br />パスワード：iwa</p>
             </div>
+            <Link-button :url="'#'" />
           </swiper-slide>
         </swiper>
       </Container>
@@ -78,7 +78,7 @@ export default {
 .slider-wrapper {
   padding-left: 11%;
 }
-.swiper-slide-text {
+.slide-text {
   padding: 0 10%;
 }
 .swiper-button-next,

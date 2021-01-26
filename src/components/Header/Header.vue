66d22f210c2e9db02e1895d83495fa2852af21b3
Yusuke-Koiwa <smallrock0109@gmail.com>
2021-01-26 21:31:44 +0900

    ハンバーガーメニュー追加

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,12 +1,21 @@
 <template>
   <header class="fixed z-50 w-full h-20 px-6 bg-gradient-to-b from-black">
     <nav class="w-full h-full flex justify-between items-center text-white">
-      <h1 class="font-heading text-4xl">Koiwa's Portfolio</h1>
-      <ul class="flex font-heading text-xl">
+      <h1 class="font-heading text-4xl whitespace-nowrap">Koiwa's Portfolio</h1>
+      <ul class="hidden lg:flex font-heading text-xl">
         <li v-for="link in links" :key="link.name" class="px-4">
           <a :href="link.url">{{ link.name }}</a>
         </li>
       </ul>
+      <div class="mobile-menu lg:hidden absolute top-0 right-0 w-24 h-24 shadow bg-c-gray">
+        <button @click="menuOpen = !menuOpen" class="absolute top-7 right-5 w-8 h-8 focus:outline-none">
+          <div class="relative w-full h-full">
+            <div class="bar absolute top-0 right-0 w-full h-1 bg-black"></div>
+            <div class="bar absolute top-2 right-0 w-full h-1 bg-black"></div>
+            <div class="bar absolute top-4 right-0 w-full h-1 bg-black"></div>
+          </div>
+        </button>
+      </div>
     </nav>
   </header>
 </template>
@@ -16,6 +25,7 @@ export default {
   name: 'Header',
   data() {
     return {
+      menuOpen: false,
       links: [
         {
           name: 'ABOUT',
@@ -42,3 +52,9 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.mobile-menu {
+  border-bottom-left-radius: 100%;
+}
+</style>

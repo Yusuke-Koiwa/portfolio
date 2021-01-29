2ab0372a2ce731a39e1f46860fb968b160b64d7e
Yusuke-Koiwa <smallrock0109@gmail.com>
2021-01-29 09:25:11 +0900

    Heroコンポーネント分割

--- src/components/Hero/Hero.vue
+++ src/components/Hero/Hero.vue
@@ -1,7 +1,6 @@
 <template>
   <section class="hero relative w-full h-screen px-6 bg-cover bg-no-repeat bg-center">
-    <img :src="imgUrl" alt="" class="absolute bottom-0 -right-0.5 w-full h-full" />
-
+    <HeroWave :darkTheme="darkTheme" />
     <p
       class="absolute catch-copy top-1/2 right-1/2 transform translate-x-3/4 -translate-y-1/2 font-heading text-7xl text-center z-10 whitespace-nowrap dark:text-white"
     >
@@ -17,26 +16,13 @@
 </template>
 
 <script>
+import HeroWave from './HeroWave.vue';
 import ScrollMark from './ScrollMark.vue';
 export default {
-  components: { ScrollMark },
-  data() {
-    return {
-      imgUrl: null,
-    };
-  },
+  components: { HeroWave, ScrollMark },
   props: {
     darkTheme: {
       type: Boolean,
-    },
-  },
-  watch: {
-    darkTheme: function (dark) {
-      if (dark) {
-        this.imgUrl = '/static/img/hero/hero-wave-black.png';
-      } else {
-        this.imgUrl = '/static/img/hero/hero-wave-white.png';
-      }
     },
   },
 };
@@ -46,11 +32,9 @@ export default {
 .hero {
   background-image: url('/static/img/hero/hero-bg@2x.jpg');
 }
-
 .catch-copy {
   text-shadow: 1px 0 10px #fff;
 }
-
 .rocket {
   width: 600px;
   /* animation: hovering 2s ease-in-out infinite alternate; */

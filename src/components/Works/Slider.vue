bd21a2cb5296f3a118d752ab14ac7f634007ffce
Yusuke-Koiwa <smallrock0109@gmail.com>
2021-01-30 11:14:06 +0900

    Works レスポンシブ化

--- src/components/Works/Slider.vue
+++ src/components/Works/Slider.vue
@@ -2,56 +2,70 @@
   <div class="slider-wrapper">
     <div class="relative">
       <div class="slider-bg absolute bottom-0 top-0 left-0 right-0 bg-c-gray dark:bg-dark-gray bg-opacity-90 dark:bg-opacity-90"></div>
-      <Container>
-        <swiper ref="mySwiper" :slides-per-view="2" :space-between="50" :loop="true" navigation class="dark:text-white">
+      <Container class="md:pl-16 2xl:pl-6">
+        <swiper
+          ref="mySwiper"
+          :slides-per-view="1"
+          :space-between="0"
+          :loop="true"
+          :breakpoints="{ 768: { slidesPerView: 2, spaceBetween: 0 } }"
+          navigation
+          class="dark:text-white"
+        >
           <swiper-slide class="flex flex-col h-auto justify-between">
-            <img src="/static/img/works/works01@2x.png" alt="" width="1264" height="580" loading="lazy" class="mb-9" />
-            <h3 class="slide-text mb-9 text-3xl text-center font-semibold">Anytime Nature</h3>
-            <div class="slide-text mb-9 text-lg">
-              <p>
-                写真を投稿できるWebアプリケーションです。 バックエンドのサーバー処理から、フロント
-                エンドの実装まで全て個人で開発しました。バックエンドはRuby on Rails、画像の保存は AWS S3を使用しています。
-              </p>
+            <div>
+              <img src="/static/img/works/works01@2x.png" alt="" width="1264" height="580" loading="lazy" class="mb-5" />
+              <div class="slide-text mx-auto md:px-6">
+                <h3 class="slide-heading mb-6 text-2xl text-center font-semibold">Anytime Nature</h3>
+                <p class="mb-5 text-lg">
+                  写真を投稿できるWebアプリケーションです。 バックエンドのサーバー処理から、フロント
+                  エンドの実装まで全て個人で開発しました。バックエンドはRuby on Rails、画像の保存は AWS S3を使用しています。
+                </p>
+              </div>
             </div>
             <Link-button :url="'https://anytime-nature.herokuapp.com/'" />
           </swiper-slide>
 
           <swiper-slide class="flex flex-col h-auto justify-between">
-            <img src="/static/img/works/works02@2x.png" alt="" width="1264" height="580" loading="lazy" class="mb-9" />
-            <h3 class="mb-9 text-3xl text-center font-semibold">完全版マハーバーラタ（模写×WordPress）</h3>
-            <div class="slide-text">
-              <p class="mb-3 text-lg">
-                実際の案件で使用されたXDデザインを元に模写したサイトです。WordPressサイト化し、ニュース、販売スケジュール、キャストなどをクライアント様側
-                で簡単に更新できるようにしました。サイトの確認には認証が必要です。
-              </p>
-              <p class="mb-3">ユーザー名：iwa<br />パスワード：iwa</p>
+            <div>
+              <img src="/static/img/works/works02@2x.png" alt="" width="1264" height="580" loading="lazy" class="mb-5" />
+              <div class="slide-text mx-auto md:px-6">
+                <h3 class="slide-heading mb-6 text-2xl text-center font-semibold">完全版マハーバーラタ（模写×WordPress）</h3>
+                <p class="mb-3 text-lg">
+                  実際の案件で使用されたXDデザインを元に模写したサイトです。WordPressサイト化し、ニュース、販売スケジュール、キャストなどをクライアント様側
+                  で簡単に更新できるようにしました。サイトの確認には認証が必要です。
+                </p>
+                <p class="mb-3">ユーザー名：iwa<br />パスワード：iwa</p>
+              </div>
             </div>
             <Link-button :url="'https://mb.iwa-portfolio.com/'" />
           </swiper-slide>
 
           <swiper-slide class="flex flex-col h-auto justify-between">
-            <img src="/static/img/works/works03@2x.png" alt="" width="1264" height="580" loading="lazy" class="mb-9" />
-            <h3 class="mb-9 text-3xl text-center font-semibold">Engress（WordPress架空企業サイト）</h3>
-            <div class="slide-text">
-              <p class="mb-3 text-lg">
-                WordPressを用いて構築した架空の企業サイトです。XDデザインを元にコーディングを行なっております。ブログ、お知らせ、料金プランなどをクライアント様側で簡単に更新できるようにしました。サイトの確認には認証が必要です。
-              </p>
-              <p class="mb-3">ユーザー名：iwa<br />パスワード：iwa</p>
+            <div>
+              <img src="/static/img/works/works03@2x.png" alt="" width="1264" height="580" loading="lazy" class="mb-5" />
+              <div class="slide-text mx-auto md:px-6">
+                <h3 class="slide-heading mb-6 text-2xl text-center font-semibold">Engress（WordPress架空企業サイト）</h3>
+                <p class="slide-text mb-3 text-lg">
+                  WordPressを用いて構築した架空の企業サイトです。XDデザインを元にコーディングを行なっております。ブログ、お知らせ、料金プランなどをクライアント様側で簡単に更新できるようにしました。サイトの確認には認証が必要です。
+                </p>
+                <p class="mb-3">ユーザー名：iwa<br />パスワード：iwa</p>
+              </div>
             </div>
             <Link-button :url="'https://iwa-portfolio.com/'" />
           </swiper-slide>
         </swiper>
       </Container>
 
-      <div class="btn-container absolute z-10 top-1/2 left-0">
+      <div class="btn-container absolute z-10 bottom-0 md:bottom-1/2 left-1/2 md:left-0">
         <button
-          class="slider-btn mr-3.5 rounded-full text-3xl font-semibold bg-gray-300 dark:bg-gray-600 dark:text-white shadow focus:outline-none"
+          class="slider-btn w-18 h-18 mr-3.5 rounded-full text-3xl font-semibold bg-gray-300 dark:bg-dark-gray dark:text-white shadow dark:shadow-white focus:outline-none"
           @click="prevSlide"
         >
           ＜
         </button>
         <button
-          class="slider-btn rounded-full text-3xl font-semibold bg-gray-300 dark:bg-gray-600 dark:text-white shadow focus:outline-none"
+          class="slider-btn w-18 h-18 rounded-full text-3xl font-semibold bg-gray-300 dark:bg-dark-gray dark:text-white shadow dark:shadow-white focus:outline-none"
           @click="nextSlide"
         >
           ＞
@@ -93,7 +107,12 @@ export default {
   padding-left: 11%;
 }
 .slide-text {
-  padding: 0 10%;
+  max-width: 500px;
+}
+@media screen and (min-width: 768px) {
+  .slide-heading {
+    min-height: 4rem;
+  }
 }
 .swiper-button-next,
 .swiper-button-prev {
@@ -103,14 +122,15 @@ export default {
 
 <style scoped>
 .slider-bg {
-  transform: translateY(35px);
+  transform: translateY(2rem);
 }
 .btn-container {
-  transform: translateX(-70%);
+  transform: translate(-50%, 175%);
 }
-.slider-btn {
-  width: 70px;
-  height: 70px;
+@media screen and (min-width: 768px) {
+  .btn-container {
+    transform: translate(-50%, 75%);
+  }
 }
 .slider-btn:active {
   transform: translateY(1px);

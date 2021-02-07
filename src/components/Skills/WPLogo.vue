82c8ad98d4ce8a315497d7c3f71b3362dd4a8ebe
Yusuke-Koiwa <smallrock0109@gmail.com>
2021-02-07 17:53:17 +0900

    Update WPLogo.vue

--- src/components/Skills/WPLogo.vue
+++ src/components/Skills/WPLogo.vue
@@ -1,18 +1,23 @@
 <template>
-  <picture v-show="!darkTheme">
-    <source type="image/webp" srcset="/img/skills/wp/wordpress@80w.webp 1x, /img/skills/wp/wordpress@160w.webp 2x" />
-    <source type="image/png" srcset="/img/skills/wp/wordpress@80w.png 1x, /img/skills/wp/wordpress@160w.png 2x" />
-    <img src="/img/skills/wp/wordpress@80w.png" width="80" height="80" alt="" loading="lazy" class="w-20 h-auto mx-auto mb-10" />
-  </picture>
-  <picture v-show="darkTheme">
-    <source type="image/webp" srcset="/img/skills/wp/wordpress-white@80w.webp 1x, /img/skills/wp/wordpress-white@160w.webp 2x" />
-    <source type="image/png" srcset="/img/skills/wp/wordpress-white@80w.png 1x, /img/skills/wp/wordpress-white@160w.png 2x" />
-    <img src="/img/skills/wp/wordpress-white@80w.png" width="80" height="80" alt="" loading="lazy" class="w-20 h-auto mx-auto mb-10" />
+  <picture>
+    <source type="image/webp" :srcset="[darkTheme ? webpSrcsetDark : webpSrcset]" />
+    <source type="image/png" :srcset="[darkTheme ? pngSrcsetDark : pngSrcset]" />
+    <img :src="[darkTheme ? imgSrcDark : imgSrc]" width="80" height="80" alt="" loading="lazy" class="w-20 h-auto mx-auto mb-10" />
   </picture>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      webpSrcset: '/img/skills/wp/wordpress@80w.webp 1x, /img/skills/wp/wordpress@160w.webp 2x',
+      pngSrcset: '/img/skills/wp/wordpress@80w.png 1x, /img/skills/wp/wordpress@160w.png 2x',
+      imgSrc: '/img/skills/wp/wordpress@80w.png',
+      webpSrcsetDark: '/img/skills/wp/wordpress-white@80w.webp 1x, /img/skills/wp/wordpress-white@160w.webp 2x',
+      pngSrcsetDark: '/img/skills/wp/wordpress-white@80w.png 1x, /img/skills/wp/wordpress-white@160w.png 2x',
+      imgSrcDark: '/img/skills/wp/wordpress-white@80w.png',
+    };
+  },
   props: {
     darkTheme: Boolean,
   },

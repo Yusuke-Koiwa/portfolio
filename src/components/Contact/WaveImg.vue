1057e1e02c2d5e30dd2b71d8746ec2274ed6f037
Yusuke-Koiwa <smallrock0109@gmail.com>
2021-02-07 19:25:07 +0900

    Contactエリアwebp対応

--- src/components/Contact/WaveImg.vue
+++ src/components/Contact/WaveImg.vue
@@ -1,10 +1,29 @@
 <template>
-  <img v-show="darkTheme" src="/img/contact-waves-dark.png" alt="" width="1279" height="110" class="w-full" />
-  <img v-show="!darkTheme" src="/img/contact-waves-normal.png" alt="" width="1279" height="110" class="w-full" />
+  <picture>
+    <source type="image/webp" media="(max-width: 560px)" :srcset="[darkTheme ? webpSrcsetSmDark : webpSrcsetSm]" />
+    <source type="image/webp" :srcset="[darkTheme ? webpSrcsetLgDark : webpSrcsetLg]" />
+    <source type="image/png" media="(max-width: 560px)" :srcset="[darkTheme ? pngSrcsetSmDark : pngSrcsetSm]" />
+    <source type="image/png" :srcset="[darkTheme ? pngSrcsetLgDark : pngSrcsetLg]" />
+    <img :src="[darkTheme ? imgSrcDark : imgSrc]" alt="" width="1279" height="110" class="w-full" />
+  </picture>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      webpSrcsetSmDark: '/img/contact/waves-dark@sm.webp 1x, /img/contact/waves-dark@lg.webp 2x',
+      webpSrcsetLgDark: '/img/contact/waves-dark@lg.webp',
+      pngSrcsetSmDark: '/img/contact/waves-dark@sm.png 1x, /img/contact/waves-dark@lg.png 2x',
+      pngSrcsetLgDark: '/img/contact/waves-dark@lg.png',
+      imgSrcDark: '/img/contact/waves-dark@sm.png',
+      webpSrcsetSm: '/img/contact/waves@sm.webp 1x, /img/contact/waves@lg.webp 2x',
+      webpSrcsetLg: '/img/contact/waves@lg.webp',
+      pngSrcsetSm: '/img/contact/waves@sm.png 1x, /img/contact/waves@lg.png 2x',
+      pngSrcsetLg: '/img/contact/waves@lg.png',
+      imgSrc: '/img/contact/waves@sm.png',
+    };
+  },
   props: {
     darkTheme: Boolean,
   },

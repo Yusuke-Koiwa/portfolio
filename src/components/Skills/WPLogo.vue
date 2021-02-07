eb3d37ce19731dd6d061e5754841c9b8a53b7fcc
Yusuke-Koiwa <smallrock0109@gmail.com>
2021-02-07 15:48:38 +0900

    wordpressロゴ webp対応

--- src/components/Skills/WPLogo.vue
+++ src/components/Skills/WPLogo.vue
@@ -1,6 +1,14 @@
 <template>
-  <img v-show="!darkTheme" src="/img/skills/wordpress.png" width="80" height="80" alt="" loading="lazy" class="w-20 h-auto mx-auto mb-10" />
-  <img v-show="darkTheme" src="/img/skills/wordpress-white.png" width="80" height="80" alt="" loading="lazy" class="w-20 h-auto mx-auto mb-10" />
+  <picture v-show="!darkTheme">
+    <source type="image/webp" srcset="/img/skills/wp/wordpress@80w.webp 1x, /img/skills/wp/wordpress@160w.webp 2x" />
+    <source type="image/png" srcset="/img/skills/wp/wordpress@80w.png 1x, /img/skills/wp/wordpress@160w.png 2x" />
+    <img src="/img/skills/wp/wordpress@80w.png" width="80" height="80" alt="" loading="lazy" class="w-20 h-auto mx-auto mb-10" />
+  </picture>
+  <picture v-show="darkTheme">
+    <source type="image/webp" srcset="/img/skills/wp/wordpress-white@80w.webp 1x, /img/skills/wp/wordpress-white@160w.webp 2x" />
+    <source type="image/png" srcset="/img/skills/wp/wordpress-white@80w.png 1x, /img/skills/wp/wordpress-white@160w.png 2x" />
+    <img src="/img/skills/wp/wordpress-white@80w.png" width="80" height="80" alt="" loading="lazy" class="w-20 h-auto mx-auto mb-10" />
+  </picture>
 </template>
 
 <script>

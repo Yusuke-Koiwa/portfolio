d5fca3ea5bf86ce0cc2e8a7a04b095273c77be57
Yusuke-Koiwa <smallrock0109@gmail.com>
2021-02-07 17:53:46 +0900

    スライダー画像コンポーネント化

---
+++ src/components/Works/SlidePicture.vue
@@ -0,0 +1,21 @@
+<template>
+  <picture>
+    <source type="image/webp" media="(max-width: 500px)" :srcset="webpSrcsetSm" />
+    <source type="image/webp" :srcset="webpSrcset" />
+    <source type="image/png" media="(max-width: 500px)" :srcset="pngSrcsetSm" />
+    <source type="image/png" :srcset="pngSrcset" />
+    <img :src="imgSrc" alt="" width="1264" height="580" loading="lazy" class="mb-5" />
+  </picture>
+</template>
+
+<script>
+export default {
+  props: {
+    webpSrcset: String,
+    webpSrcsetSm: String,
+    pngSrcset: String,
+    pngSrcsetSm: String,
+    imgSrc: String,
+  },
+};
+</script>
--- src/components/Works/Slider.vue
+++ src/components/Works/Slider.vue
@@ -14,17 +14,13 @@
         >
           <swiper-slide class="flex flex-col h-auto justify-between">
             <div>
-              <picture>
-                <source
-                  type="image/webp"
-                  media="(max-width: 500px)"
-                  srcset="/img/works/03/works03@500w.webp 1x, /img/works/03/works03@1000w.webp 2x"
-                />
-                <source type="image/webp" srcset="/img/works/03/works03@768w.webp 1x, /img/works/03/works03@1500w.webp 2x" />
-                <source type="image/png" srcset="/img/works/03/works03@768w.png 1x, /img/works/03/works03@1500w.png 2x" />
-                <source type="image/png" media="(max-width: 500px)" srcset="/img/works/03/works03@500w.png 1x, /img/works/03/works03@1000w.png 2x" />
-                <img src="/img/works/03/works03@768w.png" alt="" width="1264" height="580" loading="lazy" class="mb-5" />
-              </picture>
+              <Slide-picture
+                webpSrcsetSm="/img/works/03/works03@500w.webp 1x, /img/works/03/works03@1000w.webp 2x"
+                webpSrcset="/img/works/03/works03@768w.webp 1x, /img/works/03/works03@1500w.webp 2x"
+                pngSrcsetSm="/img/works/03/works03@500w.png 1x, /img/works/03/works03@1000w.png 2x"
+                pngSrcset="/img/works/03/works03@768w.png 1x, /img/works/03/works03@1500w.png 2x"
+                imgSrc="/img/works/03/works03@768w.png"
+              />
               <div class="slide-text mx-auto md:px-6">
                 <h3 class="slide-heading mb-6 text-2xl text-center font-semibold">Engress</h3>
                 <p class="slide-text mb-3 text-lg">
@@ -38,17 +34,13 @@
 
           <swiper-slide class="flex flex-col h-auto justify-between">
             <div>
-              <picture>
-                <source
-                  type="image/webp"
-                  media="(max-width: 500px)"
-                  srcset="/img/works/02/works02@500w.webp 1x, /img/works/02/works02@1000w.webp 2x"
-                />
-                <source type="image/webp" srcset="/img/works/02/works02@768w.webp 1x, /img/works/02/works02@1500w.webp 2x" />
-                <source type="image/png" srcset="/img/works/02/works02@768w.png 1x, /img/works/02/works02@1500w.png 2x" />
-                <source type="image/png" media="(max-width: 500px)" srcset="/img/works/02/works02@500w.png 1x, /img/works/02/works02@1000w.png 2x" />
-                <img src="/img/works/02/works02@768w.png" alt="" width="1264" height="580" loading="lazy" class="mb-5" />
-              </picture>
+              <Slide-picture
+                webpSrcsetSm="/img/works/02/works02@500w.webp 1x, /img/works/02/works02@1000w.webp 2x"
+                webpSrcset="/img/works/02/works02@768w.webp 1x, /img/works/02/works02@1500w.webp 2x"
+                pngSrcsetSm="/img/works/02/works02@500w.png 1x, /img/works/02/works02@1000w.png 2x"
+                pngSrcset="/img/works/02/works02@768w.png 1x, /img/works/02/works02@1500w.png 2x"
+                imgSrc="/img/works/02/works02@768w.png"
+              />
               <div class="slide-text mx-auto md:px-6">
                 <h3 class="slide-heading mb-6 text-2xl text-center font-semibold">完全版マハーバーラタ</h3>
                 <p class="mb-3 text-lg">
@@ -62,17 +54,13 @@
 
           <swiper-slide class="flex flex-col h-auto justify-between">
             <div>
-              <picture>
-                <source
-                  type="image/webp"
-                  media="(max-width: 500px)"
-                  srcset="/img/works/01/works01@500w.webp 1x, /img/works/01/works01@1000w.webp 2x"
-                />
-                <source type="image/webp" srcset="/img/works/01/works01@768w.webp 1x, /img/works/01/works01@1500w.webp 2x" />
-                <source type="image/png" srcset="/img/works/01/works01@768w.png 1x, /img/works/01/works01@1500w.png 2x" />
-                <source type="image/png" media="(max-width: 500px)" srcset="/img/works/01/works01@500w.png 1x, /img/works/01/works01@1000w.png 2x" />
-                <img src="/img/works/01/works01@768w.png" alt="" width="1264" height="580" loading="lazy" class="mb-5" />
-              </picture>
+              <Slide-picture
+                webpSrcsetSm="/img/works/01/works01@500w.webp 1x, /img/works/01/works01@1000w.webp 2x"
+                webpSrcset="/img/works/01/works01@768w.webp 1x, /img/works/01/works01@1500w.webp 2x"
+                pngSrcsetSm="/img/works/01/works01@500w.png 1x, /img/works/01/works01@1000w.png 2x"
+                pngSrcset="/img/works/01/works01@768w.png 1x, /img/works/01/works01@1500w.png 2x"
+                imgSrc="/img/works/01/works01@768w.png"
+              />
               <div class="slide-text mx-auto md:px-6">
                 <h3 class="slide-heading mb-6 text-2xl text-center font-semibold">Anytime Nature</h3>
                 <p class="mb-5 text-lg">
@@ -87,17 +75,13 @@
 
           <swiper-slide class="flex flex-col h-auto justify-between">
             <div>
-              <picture>
-                <source
-                  type="image/webp"
-                  media="(max-width: 500px)"
-                  srcset="/img/works/04/works04@500w.webp 1x, /img/works/04/works04@1000w.webp 2x"
-                />
-                <source type="image/webp" srcset="/img/works/04/works04@768w.webp 1x, /img/works/04/works04@1500w.webp 2x" />
-                <source type="image/png" srcset="/img/works/04/works04@768w.png 1x, /img/works/04/works04@1500w.png 2x" />
-                <source type="image/png" media="(max-width: 500px)" srcset="/img/works/04/works04@500w.png 1x, /img/works/04/works04@1000w.png 2x" />
-                <img src="/img/works/04/works04@768w.png" alt="" width="1264" height="580" loading="lazy" class="mb-5" />
-              </picture>
+              <Slide-picture
+                webpSrcsetSm="/img/works/04/works04@500w.webp 1x, /img/works/04/works04@1000w.webp 2x"
+                webpSrcset="/img/works/04/works04@768w.webp 1x, /img/works/04/works04@1500w.webp 2x"
+                pngSrcsetSm="/img/works/04/works04@500w.png 1x, /img/works/04/works04@1000w.png 2x"
+                pngSrcset="/img/works/04/works04@768w.png 1x, /img/works/04/works04@1500w.png 2x"
+                imgSrc="/img/works/04/works04@768w.png"
+              />
               <div class="slide-text mx-auto md:px-6">
                 <h3 class="slide-heading mb-6 text-2xl text-center font-semibold">Koiwa's Portfolio</h3>
                 <p class="mb-5 text-lg">
@@ -121,6 +105,7 @@ import 'swiper/components/navigation/navigation.scss';
 import Container from '../Container.vue';
 import LinkButton from './LinkButton.vue';
 import SlideBtn from './SlideBtn.vue';
+import SlidePicture from './SlidePicture.vue';
 SwiperCore.use([Navigation]);
 
 export default {
@@ -130,6 +115,7 @@ export default {
     Container,
     LinkButton,
     SlideBtn,
+    SlidePicture,
   },
   methods: {
     prevSlide() {

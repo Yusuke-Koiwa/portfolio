411f5b20112cedfb813c0b69136a4deafc739552
Yusuke-Koiwa <smallrock0109@gmail.com>
2021-02-14 12:06:01 +0900

    Update HeroRocket.vue

--- src/components/Hero/HeroRocket.vue
+++ src/components/Hero/HeroRocket.vue
@@ -1,9 +1,23 @@
 <template functional>
   <picture>
-    <source type="image/webp" media="(max-width: 445px)" srcSet="/img/hero/rocket/rocket@400w.webp 1x, /img/hero/rocket/rocket@800w.webp 2x" />
-    <source type="image/webp" srcSet="/img/hero/rocket/rocket@600w.webp 1x, /img/hero/rocket/rocket@1200w.webp 2x" />
-    <source type="image/png" media="(max-width: 445px)" srcSet="/img/hero/rocket/rocket@400w.png 1x, /img/hero/rocket/rocket@800w.png 2x" />
-    <source type="image/png" srcSet="/img/hero/rocket/rocket@600w.png 1x, /img/hero/rocket/rocket@1200w.png 2x" />
-    <img src="/img/hero/rocket/rocket@600w.png" alt="" width="600" height="520" />
+    <source
+      type="image/webp"
+      srcset="
+        /img/hero/rocket/rocket@400w.webp   400w,
+        /img/hero/rocket/rocket@600w.webp   600w,
+        /img/hero/rocket/rocket@800w.webp   800w,
+        /img/hero/rocket/rocket@1200w.webp 1200w
+      "
+    />
+    <source
+      type="image/png"
+      srcset="
+        /img/hero/rocket/rocket@400w.png   400w,
+        /img/hero/rocket/rocket@600w.png   600w,
+        /img/hero/rocket/rocket@800w.png   800w,
+        /img/hero/rocket/rocket@1200w.png 1200w
+      "
+    />
+    <img src="/img/hero/rocket/rocket@600w.png" alt="" width="600" height="520" sizes="(max-width: 670px) 90vw, 600px" />
   </picture>
 </template>
